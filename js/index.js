93b02f261b043599feb3f789980be48167bb12dc
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-04-01 05:10:32 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // your code here
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+
+function showRepositories() {
+  console.log(this.responseText);
+  let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
+}
